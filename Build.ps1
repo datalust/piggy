b406900b7657f52ac66102d451844055e41f35e5
Tod Thomson <tod@datalust.co>
2023-04-20 08:50:53 +1000

    Ash said to target net7.0

--- Build.ps1
+++ Build.ps1
@@ -18,14 +18,14 @@ function Publish-Gzips($version)
 {
 	$rids = @("linux-x64", "win-x64", "osx-x64")
 	foreach ($rid in $rids) {
-		& dotnet publish src/Datalust.Piggy/Datalust.Piggy.csproj -c Release -f net6.0 -r $rid /p:VersionPrefix=$version /p:PublishSingleFile=true /p:SelfContained=true /p:PublishTrimmed=true
+		& dotnet publish src/Datalust.Piggy/Datalust.Piggy.csproj -c Release -f net7.0 -r $rid /p:VersionPrefix=$version /p:PublishSingleFile=true /p:SelfContained=true /p:PublishTrimmed=true
 	    if($LASTEXITCODE -ne 0) { exit 4 }
 
 		# Make sure the archive contains a reasonable root filename
-		mv ./src/Datalust.Piggy/bin/Release/net6.0/$rid/publish/ ./src/Datalust.Piggy/bin/Release/net6.0/$rid/piggy-$version-$rid/
+		mv ./src/Datalust.Piggy/bin/Release/net7.0/$rid/publish/ ./src/Datalust.Piggy/bin/Release/net7.0/$rid/piggy-$version-$rid/
 
         if ($rid -ne "win-x64") {
-            & ./build/7-zip/7za.exe a -ttar piggy-$version-$rid.tar ./src/Datalust.Piggy/bin/Release/net6.0/$rid/piggy-$version-$rid/
+            & ./build/7-zip/7za.exe a -ttar piggy-$version-$rid.tar ./src/Datalust.Piggy/bin/Release/net7.0/$rid/piggy-$version-$rid/
             if($LASTEXITCODE -ne 0) { exit 5 }
 
             & ./build/7-zip/7za.exe a -tgzip ./artifacts/piggy-$version-$rid.tar.gz piggy-$version-$rid.tar
@@ -33,12 +33,12 @@ function Publish-Gzips($version)
 
     		rm piggy-$version-$rid.tar
         } else {
-            & ./build/7-zip/7za.exe a -tzip ./artifacts/piggy-$version-$rid.zip ./src/Datalust.Piggy/bin/Release/net6.0/$rid/piggy-$version-$rid/
+            & ./build/7-zip/7za.exe a -tzip ./artifacts/piggy-$version-$rid.zip ./src/Datalust.Piggy/bin/Release/net7.0/$rid/piggy-$version-$rid/
             if($LASTEXITCODE -ne 0) { exit 7 }
         }
 
 		# Back to the original directory name
-		mv ./src/Datalust.Piggy/bin/Release/net6.0/$rid/piggy-$version-$rid/ ./src/Datalust.Piggy/bin/Release/net6.0/$rid/publish/
+		mv ./src/Datalust.Piggy/bin/Release/net7.0/$rid/piggy-$version-$rid/ ./src/Datalust.Piggy/bin/Release/net7.0/$rid/publish/
 	}
 }
 
